76fda7c6c7d504705a3ca99da0ecc4827b1ced6d
Code Sandbox <hello@codesandbox.io>
2022-04-29 11:45:21 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5cc231a8e0d0d37dc77bbb94bb215ff9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69023e2f6c9ad7486f106664209c3f66",
+    "url": "https://.github.io/csb-s5qh0b/index.html"
+  },
+  {
+    "revision": "a409ccb13d5d8088b88e",
+    "url": "https://.github.io/csb-s5qh0b/static/css/2.670db9bb.chunk.css"
+  },
+  {
+    "revision": "a409ccb13d5d8088b88e",
+    "url": "https://.github.io/csb-s5qh0b/static/js/2.7fb0368a.chunk.js"
+  },
+  {
+    "revision": "a557cd03b3ed65afb084",
+    "url": "https://.github.io/csb-s5qh0b/static/js/main.dd7c05f2.chunk.js"
+  },
+  {
+    "revision": "123bd85aa7bc1ea78aa1",
+    "url": "https://.github.io/csb-s5qh0b/static/js/runtime~main.cf1806af.js"
+  },
+  {
+    "revision": "5d295cc139e67f13edc871f07f75047f",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-16.5d295cc1.woff2"
+  },
+  {
+    "revision": "81ba2c29edca4323c5363c2113f4455d",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-16.81ba2c29.ttf"
+  },
+  {
+    "revision": "edafa1f4fc6228bf8797dd3262f71af4",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-16.edafa1f4.woff"
+  },
+  {
+    "revision": "ffd4027de345feee47ceb06bf779ae51",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-16.ffd4027d.eot"
+  },
+  {
+    "revision": "0144a62006a1c3fc23b1420e7fbbca61",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-20.0144a620.eot"
+  },
+  {
+    "revision": "3916b23bd8f0e47c28abb7f63ad55d6c",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-20.3916b23b.ttf"
+  },
+  {
+    "revision": "5014bff33a089bf8b6dddd85cde567b6",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-20.5014bff3.woff"
+  },
+  {
+    "revision": "60937408ab08cbf425490293d2f3b8b6",
+    "url": "https://.github.io/csb-s5qh0b/static/media/blueprint-icons-20.60937408.woff2"
+  }
+]);
